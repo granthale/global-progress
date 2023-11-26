6e19a03f566b3b527fdd131ec067e3fcddf98538
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-11-26 11:25:36 +0700

    Initial Commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,12 +11,12 @@ const Header = () => {
       <Container className={styles.headerContainer}>
         <p className={styles.headerTitle}>
           <Link href="/">
-            Next.js Leaflet Starter
+            Global Progress
           </Link>
         </p>
         <ul className={styles.headerLinks}>
           <li>
-            <a href="https://github.com/colbyfayock/next-leaflet-starter" rel="noreferrer">
+            <a href="" rel="noreferrer">
               <FaGithub />
             </a>
           </li>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import Layout from '@components/Layout';
 import Section from '@components/Section';
 import Container from '@components/Container';
 import Map from '@components/Map';
-import Button from '@components/Button';
 
 import styles from '@styles/Home.module.scss';
 
@@ -14,15 +13,15 @@ export default function Home() {
   return (
     <Layout>
       <Head>
-        <title>Next.js Leaflet Starter</title>
-        <meta name="description" content="Create mapping apps with Next.js Leaflet Starter" />
+        <title>Global Progress</title>
+        <meta name="description" content="Firsthand notes on travel" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Section>
         <Container>
           <h1 className={styles.title}>
-            Next.js Leaflet Starter
+            Global Progress
           </h1>
 
           <Map className={styles.homeMap} width="800" height="400" center={DEFAULT_CENTER} zoom={12}>
@@ -40,14 +39,6 @@ export default function Home() {
               </>
             )}
           </Map>
-
-          <p className={styles.description}>
-            <code className={styles.code}>npx create-next-app -e https://github.com/colbyfayock/next-leaflet-starter</code>
-          </p>
-
-          <p className={styles.view}>
-            <Button href="https://github.com/colbyfayock/next-leaflet-starter">Vew on GitHub</Button>
-          </p>
         </Container>
       </Section>
     </Layout>
