252d5811767cd252bd843fa76cad5f9f32cc6791
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-11-26 18:47:58 +0700

    Eliminated overflow hidden

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import Head from 'next/head';
 
 import Layout from '@components/Layout';
-import Section from '@components/Section';
-import Container from '@components/Container';
 import Map from '@components/Map';
 
 import styles from '@styles/Home.module.scss';
@@ -13,31 +11,25 @@ export default function Home() {
   return (
     <Layout>
       <Head>
-        <title>Worldwide Steppers</title>
-        <meta name="description" content="Firsthand notes on travel" />
+        <title>Global Progress</title>
+        <meta name="description" content="Firsthand notes on global travel" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <Section>
-        <Container>
-
-          <Map className={styles.homeMap} width="100" height="100" center={DEFAULT_CENTER} zoom={12}>
-            {({ TileLayer, Marker, Popup }) => (
-              <>
-                <TileLayer
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                />
-                <Marker position={DEFAULT_CENTER}>
-                  <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                  </Popup>
-                </Marker>
-              </>
-            )}
-          </Map>
-        </Container>
-      </Section>
+      <Map className={styles.homeMap} width="100" height="100" center={DEFAULT_CENTER} zoom={12}>
+        {({ TileLayer, Marker, Popup }) => (
+          <>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            />
+            <Marker position={DEFAULT_CENTER}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </>
+        )}
+      </Map>
     </Layout>
   )
 }
